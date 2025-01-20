01f827eb88243c115e0e949837e0ec28d5337c22
CristianStirbu <cristian.stirbu@edu.futuregames.nu>
2025-01-20 14:08:58 +0100

    tings

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,22 +1,59 @@
+using Events;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class CameraController : EventHandler
+
+namespace Game
 {
-   
-
-   
-
-    void Start()
+    [RequireComponent(typeof(Camera))]
+    public class CameraController : EventHandler
     {
-        
-    }
+        public abstract class CameraEvent: GameEvent
+        {
+            private CameraController C_controller;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            public CameraEvent(CameraController controller)
+            {
+                C_controller = controller;
+            }
+
+            public override bool IsDone()
+            {
+                return false;
+            }
+
+        }
+
+        [SerializeField, Range(0.0f, 1.0f)]
+        public float Angle = 0.0f;
+
+        [SerializeField, Range(1.0f, 10.0f)]
+        public float Distance = 4.0f;
+
+        [SerializeField, Range(1, 10)]
+        public int SmoothingIterations = 1;
+
+        public const float MaxAngle = 70.0f;
+
+        private Camera camera;
+        private int LevelMask;
+        private static CameraController instance;
+
+        private void OnEnable()
+        {
+            camera = GetComponent<Camera>();
+            LevelMask = LayerMask.GetMask(new string[] { "level" });
+            //PushEvent(new ExplorationCameraEvent(this));
+            instance = this;
+        }
+
+        private void OnDisable()
+        {
+            instance = (instance == this ? null : instance);
+        }
+
     }
 }
+
--- Assets/Scripts/EventHandler.cs
+++ Assets/Scripts/EventHandler.cs
@@ -2,178 +2,183 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventHandler : MonoBehaviour
+
+namespace Events
 {
-    public interface IEvent
+    public class EventHandler : MonoBehaviour
     {
-        void OnBegin(bool bFirstTime);
-        void OnUpdate();
-        void OnEnd();
-        bool IsDone();
-    }
-
-    public abstract class GameEvent : IEvent
-    {
-        public virtual void OnBegin(bool bFirstTime)
+        public interface IEvent
         {
+            void OnBegin(bool bFirstTime);
+            void OnUpdate();
+            void OnEnd();
+            bool IsDone();
         }
 
-        public virtual void OnUpdate()
+        public abstract class GameEvent : IEvent
         {
-        }
-
-        public virtual void OnEnd()
-        {
-        }
-
-        public virtual bool IsDone()
-        {
-            return true;
-        }
-
-        public override string ToString()
-        {
-            return GetType().Name;
-        }
-    }
-
-    public abstract class GameEventBehaviour : MonoBehaviour, IEvent
-    {
-        public virtual void OnBegin(bool bFirstTime)
-        {
-        }
-
-        public virtual void OnUpdate()
-        {
-        }
-
-        public virtual void OnEnd()
-        {
-        }
-
-        public virtual bool IsDone()
-        {
-            return true;
-        }
-
-        public override string ToString()
-        {
-            return GetType().Name;
-        }
-    }
-    private List<IEvent> eventStack = new List<IEvent>();
-    private HashSet<IEvent> startedEvent = new HashSet<IEvent>();
-    private IEvent currentEvent;
-
-    private static EventHandler main= null;
-    #region Properties
-
-    public IEvent CurrentEvent => currentEvent;
-    public List<IEvent> EventStack => eventStack;
-
-    public static EventHandler Main
-    {
-        get
-        {
-            if (main == null&& Application.isPlaying)
+            public virtual void OnBegin(bool bFirstTime)
             {
-                GameObject go = new GameObject("MainEventHandler");
-                DontDestroyOnLoad(go);
-                main = go.AddComponent<EventHandler>();
             }
 
-            return main;
-        }
-    }
-
-    #endregion
-
-    public void PushEvent(IEvent evt)
-    {
-        if (evt != null)
-        {
-            eventStack.RemoveAll(e => e == evt);
-
-            eventStack.Insert(0, evt);
-
-            if (currentEvent != null && currentEvent != evt)
+            public virtual void OnUpdate()
             {
-                currentEvent = null;
+            }
+
+            public virtual void OnEnd()
+            {
+            }
+
+            public virtual bool IsDone()
+            {
+                return true;
+            }
+
+            public override string ToString()
+            {
+                return GetType().Name;
             }
         }
-    }
 
-    public void RemoveEvent(IEvent evt)
-    { 
-      if(evt == null || eventStack.Contains(evt))
-      {
-            return;
-      }
-
-      if(evt == currentEvent || startedEvent.Contains(evt))
-      {
-            evt.OnEnd();
-            currentEvent = null;
-      }
-
-      eventStack.Remove(evt);
-    }
-
-    protected virtual void Upgrade() 
-    {
-        UpdateEvent();
-    }
-
-    protected virtual void UpdateEvent()
-    {
-        if(eventStack.Count == 0)
+        public abstract class GameEventBehaviour : MonoBehaviour, IEvent
         {
-            return;
+            public virtual void OnBegin(bool bFirstTime)
+            {
+            }
+
+            public virtual void OnUpdate()
+            {
+            }
+
+            public virtual void OnEnd()
+            {
+            }
+
+            public virtual bool IsDone()
+            {
+                return true;
+            }
+
+            public override string ToString()
+            {
+                return GetType().Name;
+            }
+        }
+        private List<IEvent> eventStack = new List<IEvent>();
+        private HashSet<IEvent> startedEvent = new HashSet<IEvent>();
+        private IEvent currentEvent;
+
+        private static EventHandler main = null;
+        #region Properties
+
+        public IEvent CurrentEvent => currentEvent;
+        public List<IEvent> EventStack => eventStack;
+
+        public static EventHandler Main
+        {
+            get
+            {
+                if (main == null && Application.isPlaying)
+                {
+                    GameObject go = new GameObject("MainEventHandler");
+                    DontDestroyOnLoad(go);
+                    main = go.AddComponent<EventHandler>();
+                }
+
+                return main;
+            }
         }
 
-        if(currentEvent == null)
-        {
-            startedEvent.RemoveWhere(evt  => evt != null);
-            currentEvent = eventStack[0];
-            bool bFirstTime =!startedEvent.Contains(currentEvent);
-            startedEvent.Add(currentEvent);
-            currentEvent.OnBegin(bFirstTime);
+        #endregion
 
-            if(eventStack!=null)
+        public void PushEvent(IEvent evt)
+        {
+            if (evt != null)
             {
-                if(eventStack.Count > 0 && currentEvent != eventStack[0])
+                eventStack.RemoveAll(e => e == evt);
+
+                eventStack.Insert(0, evt);
+
+                if (currentEvent != null && currentEvent != evt)
                 {
                     currentEvent = null;
-                    UpdateEvent();
                 }
             }
         }
-    }
 
-    protected virtual void OnGUI()
-    {
-        if(this != main)
+        public void RemoveEvent(IEvent evt)
         {
-            return;
+            if (evt == null || eventStack.Contains(evt))
+            {
+                return;
+            }
+
+            if (evt == currentEvent || startedEvent.Contains(evt))
+            {
+                evt.OnEnd();
+                currentEvent = null;
+            }
+
+            eventStack.Remove(evt);
         }
+
+        protected virtual void Upgrade()
+        {
+            UpdateEvent();
+        }
+
+        protected virtual void UpdateEvent()
+        {
+            if (eventStack.Count == 0)
+            {
+                return;
+            }
+
+            if (currentEvent == null)
+            {
+                startedEvent.RemoveWhere(evt => evt != null);
+                currentEvent = eventStack[0];
+                bool bFirstTime = !startedEvent.Contains(currentEvent);
+                startedEvent.Add(currentEvent);
+                currentEvent.OnBegin(bFirstTime);
+
+                if (eventStack != null)
+                {
+                    if (eventStack.Count > 0 && currentEvent != eventStack[0])
+                    {
+                        currentEvent = null;
+                        UpdateEvent();
+                    }
+                }
+            }
+        }
+
+        protected virtual void OnGUI()
+        {
+            if (this != main)
+            {
+                return;
+            }
 
 #if UNITY_EDITOR
 
-        const float lineHight = 32.0f;
+            const float lineHight = 32.0f;
 
-        GUI.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
-        Rect r = new Rect(0,0,250.0f,lineHight*eventStack.Count);
-        GUI.DrawTexture(r, Texture2D.whiteTexture);
+            GUI.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+            Rect r = new Rect(0, 0, 250.0f, lineHight * eventStack.Count);
+            GUI.DrawTexture(r, Texture2D.whiteTexture);
 
-        Rect line = new Rect(10,0,r.width-20,lineHight);
-        for(int i = 0; i < eventStack.Count; i++)
-        {
-            GUI.color = eventStack[i] == currentEvent ? Color.green : Color.white;
-            GUI.Label(line,"g"+ i+":"+ eventStack[i].ToString(),i ==0? UnityEditor.EditorStyles.boldLabel : UnityEditor.EditorStyles.label);
-            line.y += line.height;
+            Rect line = new Rect(10, 0, r.width - 20, lineHight);
+            for (int i = 0; i < eventStack.Count; i++)
+            {
+                GUI.color = eventStack[i] == currentEvent ? Color.green : Color.white;
+                GUI.Label(line, "g" + i + ":" + eventStack[i].ToString(), i == 0 ? UnityEditor.EditorStyles.boldLabel : UnityEditor.EditorStyles.label);
+                line.y += line.height;
+            }
+
+#endif
         }
 
-     #endif
     }
 
 }
