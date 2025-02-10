eb0feb2a1f1d9c284d55144a79c33bcd395e9848
CristianStirbu <145765962+CristianStirbu@users.noreply.github.com>
2025-02-10 14:18:28 +0100

    more work

--- Assets/Scripts/CameraTarget.cs
+++ Assets/Scripts/CameraTarget.cs
@@ -22,7 +22,12 @@ namespace Game
         void Update()
         {
             Vector3 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
-            Vector3 targetPos = (player.position + mousePos) / 2f;
+            Vector3 targetPos = (player.position + mousePos)/2f;
+
+            targetPos.x = Mathf.Clamp(targetPos.x,-threshold+player.position.x,threshold+ player.position.x);
+            targetPos.y = Mathf.Clamp(targetPos.y, -threshold + player.position.y, threshold + player.position.y);
+
+            this.transform.position = targetPos;
         }
     }
 
